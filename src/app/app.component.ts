7364b82b042d83acd63d9298dbde50b84bec1efe
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-11-19 16:01:55 -0600

    Part One Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeDataService} from './poke-data.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PokeDataService]
 })
-export class AppComponent {
-  title = 'poke-tutorial';
+export class AppComponent implements OnInit{
+  poke: any;
+  url = 'https://pokeapi.co/api/v2/pokemon/growlithe/'
+  constructor(private srv: PokeDataService){
+
+  }
+  getPoke():void{
+    this.srv.getData(this.url).subscribe(data=>this.poke=data)
+    console.log(this.poke)
+  }
+  ngOnInit(){
+    this.getPoke()
+  }
 }
+
--- src/app/poke-data.service.ts
+++ src/app/poke-data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeDataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getData(url:string){
+    console.log(url)
+    console.log(this.http.get(url))
+    return this.http.get(url)
+  }
 }
