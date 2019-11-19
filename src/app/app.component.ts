60c857166868c7242b62411c432edc35500d057b
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-11-19 16:17:10 -0600

    Finished Part Two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,18 @@ export class AppComponent implements OnInit{
   poke: any;
   url = 'https://pokeapi.co/api/v2/pokemon/growlithe/'
   constructor(private srv: PokeDataService){
+    let self = this;
+    window.onload=function(){
+      let btn = document.getElementById("iChooseYou");
+      btn.addEventListener("click", (e:Event) => self.changeURL());
 
+    }
+  }
+  changeURL():void{
+    let pokemon2use = (<HTMLInputElement>document.getElementById("Pokemon2Get")).value
+    this.url = 'https://pokeapi.co/api/v2/pokemon/'+ pokemon2use + '/'
+    console.log(this.url)
+    this.getPoke()
   }
   getPoke():void{
     this.srv.getData(this.url).subscribe(data=>this.poke=data)
